6872ecf19a4a2ff57bd9ed3cc4b58a8cc5476abf
Michelle Willcox <shellwillcox@gmail.com>
2019-10-08 10:34:01 -0700

    Updates

---
+++ precache-manifest.c6d3ae4369ac4b1c88f727b974c11ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01bfe923d5abc9afc7d7cfbc12c194b",
+    "url": "/remix/index.html"
+  },
+  {
+    "revision": "560fd340f627bd82d3e3",
+    "url": "/remix/static/css/main.ceeb8642.chunk.css"
+  },
+  {
+    "revision": "ee29512769ad7b613685",
+    "url": "/remix/static/js/2.bbc4a40a.chunk.js"
+  },
+  {
+    "revision": "560fd340f627bd82d3e3",
+    "url": "/remix/static/js/main.0fd0f3dd.chunk.js"
+  },
+  {
+    "revision": "3935a2cd31d9595db49d",
+    "url": "/remix/static/js/runtime-main.5e616c27.js"
+  }
+]);
